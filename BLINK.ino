f347a84ee05f31760bcab04dfea33d0792fce73f
DaFloW19 <105713475+DaFloW19@users.noreply.github.com>
2023-05-25 16:32:00 +0100

    Module 1
    module 1

---
+++ BLINK.ino
@@ -0,0 +1,14 @@
+void setup(void)
+{
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop(void)
+{
+  digitalWrite(LED_BUILTIN, HIGH);   
+  delay(1000);                       
+  digitalWrite(LED_BUILTIN, LOW);    
+  delay(1000);                       
+}
